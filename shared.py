f664eec0e052a121f404f8f644c9b43eb79957ba
András Ottó Földes <aof26@cam.ac.uk>
2024-09-02 12:20:54 +0200

    Async IO rewrite (#1)
    * First move to async Slack API
    
    * Adjusting parrotmaker.py
    
    * Re-implementing the gdrive sync & replaced shell execution with subprocesses and aiohttp
    
    * Re-done the live archive with async API & fixed locking to apply to all file accesses
    
    * Update config and README.md
    
    * Finish renaming

--- shared.py
+++ shared.py
@@ -1,21 +1,39 @@
-import yaml
+import asyncio
 import sys
 import argparse
-from slack_bolt import App
+
+import yaml
+from slack_bolt.app.async_app import AsyncApp
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--journald', action='store_true',
                     help='Use systemd journal outputs instead of stdout and stderr')
 parser.add_argument('-c', '--config',
                     help='Specify a path for a config.yaml',
-                    default='/etc/slack-parrotbot/config.yaml')
+                    default='/etc/async-parrotbot/config.yaml')
 args = parser.parse_args()
+
+async def async_check_output(cmd, **kwargs):
+	process = await asyncio.create_subprocess_shell(
+		cmd,
+		stdout=asyncio.subprocess.PIPE,
+		stderr=asyncio.subprocess.PIPE,
+		**kwargs
+	)
+	stdout, stderr = await process.communicate()
+	if process.returncode == 0:
+		return stdout
+	else:
+		print(f"Error occured while executing command "
+			  f"`{cmd}` during /parrotcheckhealth:", file=err_stream)
+		print(stderr, file=err_stream)
 
 if args.journald:
 	from systemd import journal
-	debug_stream  = journal.stream('slack-parrotbot', priority=7)
-	info_stream   = journal.stream('slack-parrotbot', priority=6)
-	warn_stream   = journal.stream('slack-parrotbot', priority=4)
-	err_stream    = journal.stream('slack-parrotbot', priority=3)
+	debug_stream  = journal.stream('async-parrotbot', priority=7)
+	info_stream   = journal.stream('async-parrotbot', priority=6)
+	warn_stream   = journal.stream('async-parrotbot', priority=4)
+	err_stream    = journal.stream('async-parrotbot', priority=3)
 
 	sys.stdout = info_stream
 	sys.stderr = err_stream
@@ -28,8 +46,8 @@ else:
 
 if args.config is not None:
 	config_file = args.config
-	if args.config == '/etc/slack-parrotbot/config.yaml':
-		print("No config file specified, defaulting to /etc/slack-parrotbot/config.yaml...",
+	if args.config == '/etc/async-parrotbot/config.yaml':
+		print("No config file specified, defaulting to /etc/async-parrotbot/config.yaml...",
 		      file = warn_stream)
 else:
 	print("argparse is broken, I am confused, abort.", file=err_stream)
@@ -38,4 +56,4 @@ else:
 
 config = yaml.load(open(config_file), Loader=yaml.loader.FullLoader)
 
-app = App(token = config['slack_bot_token'])
+slack_app = AsyncApp(token = config['slack_bot_token'])
